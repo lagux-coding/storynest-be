97a0ee397db6308008444d435e4f88de67c139e7
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-08-27 13:06:34 +0700

    init database

---
+++ StoryNest.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryNest.Domain.Entities
+{
+    public partial class User
+    {
+        [Column("id")]
+        [Key]
+        public int Id { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; } = null!;
+
+        [Column("password_hash")]
+        public string PasswordHash { get; set; } = null!;
+    }
+}
---
+++ StoryNest.Infrastructure/Persistence/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoryNest.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryNest.Infrastructure.Persistence
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
