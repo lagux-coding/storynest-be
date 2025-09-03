c027297169fdb39aa2560a3a35cb86a4c0499659
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-09-03 17:42:38 +0700

    update new migration for refreshToken table

---
+++ StoryNest.Infrastructure/Migrations/20250903104022_V1_UpdateRefreshTokenTbl.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoryNest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class V1_UpdateRefreshTokenTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "token",
+                table: "RefreshTokens",
+                newName: "tokenHash");
+
+            migrationBuilder.AddColumn<string>(
+                name: "JwtId",
+                table: "RefreshTokens",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "device_id",
+                table: "RefreshTokens",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ip_address",
+                table: "RefreshTokens",
+                type: "character varying(45)",
+                maxLength: 45,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "replaced_by_token_hash",
+                table: "RefreshTokens",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "user_agent",
+                table: "RefreshTokens",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_tokenHash",
+                table: "RefreshTokens",
+                column: "tokenHash",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_tokenHash",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "JwtId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "device_id",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "ip_address",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "replaced_by_token_hash",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "user_agent",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameColumn(
+                name: "tokenHash",
+                table: "RefreshTokens",
+                newName: "token");
+        }
+    }
+}
