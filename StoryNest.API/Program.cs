551be61e1ea93e6a445b32d140ff327b85a6d762
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-09-09 16:46:43 +0700

    update new github acton

--- StoryNest.API/Program.cs
+++ StoryNest.API/Program.cs
@@ -59,10 +59,16 @@ foreach (System.Collections.DictionaryEntry de in Environment.GetEnvironmentVari
 }
 
 // Redis DI
+var redisHost = builder.Configuration["Redis:Host"] ?? "localhost";
+var redisPort = builder.Configuration["Redis:Port"] ?? "6379";
+var redisPassword = builder.Configuration["REDIS_PASSWORD"];
 var redisConnection = builder.Configuration["REDIS_CONNECTION_STRING"];
 builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
 {
-    var redisConnection = builder.Configuration["REDIS_CONNECTION_STRING"];
+    if (redisHost != "localhost" || redisPassword != null)
+    {
+        redisConnection = redisConnection = $"{redisHost}:{redisPort},password={redisPassword},abortConnect=false";
+    }
     return ConnectionMultiplexer.Connect(redisConnection);
 });
 
