e4595337b7b2e2a427fd299c1f91ebe899381543
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-09-10 14:43:04 +0700

    fix/change secure https

--- StoryNest.API/Program.cs
+++ StoryNest.API/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:3000")
+            .WithOrigins("https://localhost:3000", "http://localhost:3000")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
