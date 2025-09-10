facf891ec51dbc6852085828616ade9808e10418
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-09-10 16:36:44 +0700

    update new cors domain

--- StoryNest.API/Program.cs
+++ StoryNest.API/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("https://localhost:3000", "http://localhost:3000")
+            .WithOrigins("https://localhost:3000", "http://localhost:3000", "https://storynest-fe.kusl.io.vn")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
