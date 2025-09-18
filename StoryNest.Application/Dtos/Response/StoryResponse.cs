c5b24a719fc6bed928b729518554e1725e1b6ec8
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-09-19 00:38:12 +0700

    update story response

--- StoryNest.Application/Dtos/Response/StoryResponse.cs
+++ StoryNest.Application/Dtos/Response/StoryResponse.cs
@@ -1,4 +1,5 @@
 ﻿using StoryNest.Domain.Entities;
+using StoryNest.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,15 @@ namespace StoryNest.Application.Dtos.Response
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
         public string CoverImageUrl { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
+        public DateTime? PublishedAt { get; set; }
         public int LikeCount { get; set; }
         public int CommentCount { get; set; }
+        public PrivacyStatus PrivacyStatus { get; set; }
+        public StoryStatus StoryStatus { get; set; }
         public UserBasicResponse User { get; set; }
         public List<MediaResponse> Media { get; set; } = new List<MediaResponse>();
         public List<TagResponse> Tags { get; set; } = new List<TagResponse>();
